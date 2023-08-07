f88e1a36edccc9d1b457a82215dbb18c70a83925
V4zha <arvin_j4848@protonmail.com>
2023-08-07 14:34:09 +0000

    Delete asts_front directory

--- asts_front/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo  "Copying Events"
-
-cp ~/mec/ast_contracts/build/contracts/ASTEvent.json ~/mec/asts_front/std_api/assets/ASTEvent.json
-
-echo  "Copying Students"
-
-cp ~/mec/ast_contracts/build/contracts/Student.json ~/mec/asts_front/std_api/assets/Student.json
